92bd399e1a655db3b6dda8a127e75fea72de6836
Pritam Raj <pritam271raj@gmail.com>
2024-06-27 12:55:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import React from 'react'
+
+const App = () => {
+  useGSAP(()=>{
+    gsap.to(".box",{
+      x: 1000,
+      duration: 2,
+      delay: 1
+    })
+  })
+  return (
+    <main>
+      <div className="box">
+
+      </div>
+    </main>
+  )
+}
+
+export default App
