d49ffe9be340bf3608127692cb46485dea85501e
Sidhant Mishra <Sidhantmishra101@gmail.com>
2024-06-28 14:11:13 +0530

    login animations error

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import './App.css';
 import Home from './pages/Home.jsx';
-import LoginSignup from './pages/LoginSignup.jsx';
+import Login from './pages/Login.jsx';
 import History from './pages/History.jsx';
 import AskAi from './pages/AskAi.jsx';
+import Register from './pages/Register.jsx';
+import Reset from './pages/Reset.jsx';
+
 
 const App = () => {
   return (
@@ -14,9 +17,11 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<LoginSignup />} />
+          <Route path="/login" element={<Login/>} />
           <Route path="/history" element={<History />} />
           <Route path="/askAi" element={<AskAi/>} />
+          <Route path='/register' element={<Register/>}/>
+          <Route path='/reset' element={<Reset/>}/>
         </Routes>
       </Router>
     </div>
