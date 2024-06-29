0af61261f26e934c1c08b73e4a75d11a8a447246
Shubham22Bisht <shubhambisht1924@gmail.com>
2024-06-29 16:03:54 +0530

    added dark mode light mode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import logoImg from "../assets/expenses.png";
 import { FaHome, FaHistory, FaUser } from "react-icons/fa";
 import { GiArtificialHive } from "react-icons/gi";
-import { MdMedicalServices } from "react-icons/md";
+import { MdMedicalServices, MdOutlineLightMode, MdOutlineDarkMode } from "react-icons/md";
 
-const Navbar = ({ isDarkMode }) => {
+const Navbar = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
     <div className={`navbar ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
       <img className='logo' src={logoImg} alt="logo" />
@@ -28,10 +34,15 @@ const Navbar = ({ isDarkMode }) => {
           <span className="nav-text">Ask AI</span>
         </Link>
       </div>
-      <Link className="nav-link login" to="/login">
-        <FaUser className="nav-icon" />
-        <span className="nav-text">Login</span>
-      </Link>
+      <div className='toggle-login-container'>
+        <button className={`toggle-button ${isDarkMode ? 'dark-mode' : 'light-mode'}`} onClick={toggleDarkMode}>
+          {isDarkMode ? <MdOutlineLightMode className="toggle-icon" /> : <MdOutlineDarkMode className="toggle-icon" />}
+        </button>
+        <Link className="nav-link login" to="/login">
+          <FaUser className="nav-icon" />
+          <span className="nav-text">Login</span>
+        </Link>
+      </div>
     </div>
   );
 };
