603d8f702cec75512563ac9cdeab09f4997e2f8a
Pritam Raj <pritam271raj@gmail.com>
2024-06-30 23:37:55 +0530

    Signup completed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Login.css';
+import loginimage from '../assets/login2.png'
 
 const Login = () => {
 
@@ -22,39 +23,44 @@ const Login = () => {
   };
 
   return (
-    <div className="form-container">
-      <p className="title">Login</p>
-      <form className="form" onSubmit={handleSubmit}>
-        <div className="input-group">
-          <label htmlFor="username">Username</label>
-          <input 
-            type="text" 
-            name="username" 
-            id="username" 
-            placeholder="username" 
-            value={username} 
-            onChange={handleUsernameChange} 
-          />
-        </div>
-        <div className="input-group">
-          <label htmlFor="password">Password</label>
-          <input 
-            type="password" 
-            name="password" 
-            id="password" 
-            placeholder="password" 
-            value={password} 
-            onChange={handlePasswordChange} 
-          />
-          <div className="forgot">
-            <a rel="noopener noreferrer" href="#">Forgot Password?</a>
+    <div className='main-div'>
+      <div className="form-container">
+        <p className="title">Login</p>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="username">Username</label>
+            <input 
+              type="text" 
+              name="username" 
+              id="username" 
+              placeholder="username" 
+              value={username} 
+              onChange={handleUsernameChange} 
+            />
           </div>
-        </div>
-        <button className="sign" type="submit" onClick={handleSubmit}>Sign in</button>
-      </form>
-      <p className="signup">Don't have an account?
-        <Link to="/signup">Signup</Link>
-      </p>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input 
+              type="password" 
+              name="password" 
+              id="password" 
+              placeholder="password" 
+              value={password} 
+              onChange={handlePasswordChange} 
+            />
+            <div className="forgot">
+              <a rel="noopener noreferrer" href="#">Forgot Password?</a>
+            </div>
+          </div>
+          <button className="sign" type="submit" onClick={handleSubmit}>Sign in</button>
+        </form>
+        <p className="signup">Don't have an account? 
+          <Link to="/signup"> Signup</Link>
+        </p>
+      </div>
+      <div className="loginimg">
+        <img className='login-image' src={loginimage} alt="" />
+      </div>
     </div>
   );
 };
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Signup.css';
+import signupimage from '../assets/login6.png'
 
 const Login = () => {
 
@@ -30,67 +31,64 @@ const Login = () => {
   };
 
   return (
-    <div className="form-container">
-      <p className="title">Signup</p>
-      <form className="form" onSubmit={handleSubmit}>
-        <div className="input-group">
-          <label htmlFor="username">Username</label>
-          <input 
-            type="text" 
-            name="username" 
-            id="username" 
-            placeholder="Username" 
-            value={username} 
-            onChange={handleUsernameChange} 
-          />
-        </div>
-
-        <div className="input-group">
-          <label htmlFor="email">Email</label>
-          <input 
-            type="email" 
-            name="email" 
-            id="email" 
-            placeholder="Email" 
-            value={email} 
-            onChange={handleEmailChange} 
-          />
-        </div>
-
-
-        <div className="input-group">
-          <label htmlFor="password">Password</label>
-          <input 
-            type="password" 
-            name="password" 
-            id="password" 
-            placeholder="Create a strong Password" 
-            value={password} 
-            onChange={handlePasswordChange} 
-          />
-          
-        </div>
-
-
-
-        <div className="input-group">
-          <label htmlFor="confirmpassword">Confirm Password</label>
-          <input 
-            type="password" 
-            name="confirmpassword" 
-            id="confirmpassword" 
-            placeholder="Confirm Your Password" 
-            value={confirmpassword} 
-            onChange={handleConfirmpasswordChange} 
-          />
-        </div>
-        
-        <button className="sign" type="submit" onClick={handleSubmit}>Create My Account</button>
-      </form>
-      <p className="signup">Already have an Account?
-        <Link to="/login"> Login</Link>
-      </p>
-    </div>
+    <div className='main-div'>
+      <div className="form-container">
+        <p className="title">Signup</p>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="username">Username</label>
+            <input 
+              type="text" 
+              name="username" 
+              id="username" 
+              placeholder="Username" 
+              value={username} 
+              onChange={handleUsernameChange} 
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input 
+              type="email" 
+              name="email" 
+              id="email" 
+              placeholder="Email" 
+              value={email} 
+              onChange={handleEmailChange} 
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input 
+              type="password" 
+              name="password" 
+              id="password" 
+              placeholder="Create a strong Password" 
+              value={password} 
+              onChange={handlePasswordChange} 
+            />      
+          </div>
+          <div className="input-group">
+            <label htmlFor="confirmpassword">Confirm Password</label>
+            <input 
+              type="password" 
+              name="confirmpassword" 
+              id="confirmpassword" 
+              placeholder="Confirm Your Password" 
+              value={confirmpassword} 
+              onChange={handleConfirmpasswordChange} 
+            />
+          </div> 
+          <button className="sign" type="submit" onClick={handleSubmit}>Create My Account</button>
+        </form>
+        <p className="signup">Already have an Account?
+          <Link to="/login"> Login</Link>
+        </p>
+      </div>
+      <div className="signupimg">
+        <img className='signup-image' src={signupimage} alt="" />
+      </div>
+    </div>  
   );
 };
 
